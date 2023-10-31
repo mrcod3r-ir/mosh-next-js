9b11f780a425ea9535e99ff5afb776db239af9b1
mrcod3r.ir <mrcod3r.ir@gmail.com>
2023-10-31 09:23:57 +0330

    implement use client & fetch on server

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button
+      onClick={() => {
+        console.log("Click");
+      }}
+    >
+      Add to cart
+    </button>
+  );
+};
+
+export default AddToCart;
